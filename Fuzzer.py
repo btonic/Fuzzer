283d41bb6bedb01d243429cfd63c5033f8211d12
ThatITNinja <ThatITNinja@hotmail.com>
2014-01-24 02:41:42 -0500

    Work on pep-8 compliance

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,99 +1,84 @@
-import fuzzer.sqliteengine as SQLEngine
+import sqliteengine as SQLEngine
 import random, datetime
 
 class Fuzzer(object):
-    def __init__(self, database = ":memory:", cache_tablenames = True, table_name = "attempts"):
-        self.sql_engine = SQLEngine.SQLiteEngine(database, tables_to_cache = cache_tablenames)
+    """
+    Fuzzer is used to either generate fuzzed values, or to read fuzzed values
+    and catch up or tail an already running fuzzer.
+    """
+    def __init__(self, database=":memory:", cache_tablenames=True,
+                 table_name="attempts"):
+        self.sql_engine = SQLEngine.SQLiteEngine(database,
+                          tables_to_cache=cache_tablenames)
         self.table_name = table_name
     def initialize(self):
         """
         Initialize the database, if there is not already one.
         """
         try:
-            self.sql_engine.create_database(self.table_name,
-                                           ("attempt_id", "INTEGER PRIMARY KEY"),
-                                           ("attempted",  "TEXT"),
-                                           ("prohibited", "TEXT"),
-                                           ("successful", "BOOL"),
-                                           ("created_at", "TEXT"),
-                                           ("updated_at", "TEXT")
-                                           )
+            self.sql_engine.create_database(
+                           self.table_name,
+                           ("attempt_id", "INTEGER PRIMARY KEY"),
+                           ("attempted", "TEXT"),
+                           ("prohibited", "TEXT"),
+                           ("successful", "BOOL"),
+                           ("created_at", "TEXT"),
+                           ("updated_at", "TEXT")
+                           )
         except SQLEngine.TableAlreadyExists:
             pass
         return True
-    def fuzz(self, random_generation = False, prohibit = None, length = 5, output_format = "'{fuzzed_string}'"):
-      prohibited_chars = False
-      if prohibit != None:
-          if type(prohibit) == type(list()):
-              prohibited_characters = list(prohibit)
-              prohibited_chars = True
-          else:
-              raise TypeError("List was expected for `prohibit` parameter.")
-      loop_template = \
-      """
-      {outer_indentation}for {loop_variable} in {iterator}:
-      {inner_indentation}{loop_code}
-      """
+    def increment(self, values, index, maximum=255, reset=True):
+        """
+        Handles incrementation for fuzzer.
+        """
+        if index >= 0:
+            if values[index]+1 >= maximum:
+                if reset:
+                    values[index] = 0
+                self.increment(values, index - 1,
+                               maximum=maximum, reset=reset)
+            else:
+                values[index] = values[index] + 1
+        return
+    def fuzz(self, random_generation=False, prohibit=None,
+                   length=5, output_format="{'fuzzed_string'}",
+                   character_evaluator=chr, maximum=255):
+        """
+        Generates all possibilities with a given length. If random is passed,
+        it will generate random values with a given length in a range between
+        0 and `maximum`. The character_evaluator will be used to convert the
+        number into its character form.
+        """
+        if maximum >= 255 and character_evaluator == chr:
+            raise TooHighForChr("maximum is too large for chr,\
+                                 must be between 0 and 255." % maximum)
+        if prohibit != None:
+            if type(prohibit) != type(list()):
+                raise TypeError("`prohibit` must be a list.")
+            else:
+                for value in prohibit:
+                    if type(value) != type(str()):
+                        raise TypeError("Values in prohibit must be a string.")
+                    else:
+                        if len(value) > 1:
+                            raise ValueError("Values in prohibit must only be\
+                                              one character long.")
+        if type(output_format) != type(str()):
+            raise TypeError("output_format should be a string.")
+        if type(random_generation) != type(bool()):
+            raise TypeError("random_generation should be a bool.")
 
-      prohibit_template = \
-      """
-      if chr({loop_variable}) in {prohibited_list}: continue;\n
-      """
-
-      conjugation_template = \
-      """
-      yield {variable_conjugation}
-      """
-
-      final = ""
-      indent = "    "
-      if not random_generation:
-          if prohibited_chars:
-              for depth_level in range(length):
-                  final += loop_template.format(
-                           outer_indentation = indent * depth_level,
-                           inner_indentation = indent * (depth_level + 1),
-                           loop_variable     = "var%s" % str(depth_level),
-                           iterator          = "range(0,256)",
-                           loop_code         = prohibit_template.format(
-                                               loop_variable   = "var%s" % str(depth_level),
-                                               prohibited_list = prohibited_characters
-                                               )\
-                                               if depth_level != length else \
-                                               \
-                                               prohibit_template.format(
-                                               loop_variable   = "var%s" % str(depth_level),
-                                               prohibited_list = prohibited_characters
-                                               ) + \
-                                               conjugation_template.format(
-                                               variable_conjugation = output_format.format(fuzzed_string = "+".join(list("chr(%s)" % ("var"+str(d_level)) for d_level in range(length))))
-                                               )
-                           )
-          else:
-              for depth_level in range(length):
-                  final += loop_template.format(
-                           outer_indentation = indent * depth_level,
-                           inner_indentation = indent * (depth_level + 1),
-                           loop_variable     = "var%s" % str(depth_level),
-                           iterator          = "range(0,256)",
-                           loop_code         = prohibit_template.format(
-                                               loop_variable   = "var%s" % str(depth_level),
-                                               prohibited_list = prohibited_characters
-                                               )\
-                                               if depth_level != length else \
-                                               conjugation_template.format(
-                                               variable_conjugation = output_format.format(fuzzed_string = "+".join(list("chr(%s)" % ("var"+str(d_level)) for d_level in range(length))))
-                                               )
-                           )
-      else:
-        if prohibited_chars:
-            for depth_level in range(length):
-                #set character to a prohibited character so that we can generate
-                #a character that is not prohibited.
-                char = prohibited_characters[0]
-                while char in prohibited_characters:
-                    char = chr(random.randrange(0,256))
-                    
+        if not random_generation:
+            done = False
+            temp_list = [0]*length
+            while not done:
+                attempt = output_format.format(fuzzed_string="".join(
+                 list(character_evaluator(character) for character in temp_list)
+                 )
+                )
+                yield Result(self, attempt, prohibit)
+                self.increment(temp_list, 0, maximum=maximum)
 
 class Result(object):
     def __init__(self, fuzzer_instance, attempt, prohibited):
@@ -101,14 +86,43 @@ class Result(object):
         self.table_name = fuzzer_instance.table_name
         self.attempt = attempt
         self.prohibited = prohibited
-        self.success = False
     def success(self):
-        self.engine_instance.append_to_pool(self._generate_item(True), self.table_name)
+        """
+        Register with the SQL engine that the result should be entered as a
+        success.
+        """
+        self.engine_instance.append_to_pool(self._generate_item(True),
+                                            self.table_name)
     def fail(self):
-        self.engine_instance.append_to_pool(self._generate_item(False), self.table_name)
+        """
+        Register with the SQL engine that the result should be entered as a
+        failure.
+        """
+        self.engine_instance.append_to_pool(self._generate_item(False),
+                                            self.table_name)
     def _generate_item(self, success_value):
+        """
+        Create the item to submit to the SQL engine.
+        """
         return {"created_at": datetime.datetime.now().strftime("%c"),
                 "updated_at": datetime.datetime.now().strftime("%c"),
                 "prohibited": self.prohibited,
                 "attempted" : self.attempt,
                 "successful": success_value}
+
+class GeneralException(Exception):
+    """
+    Baseclass for all exceptions raised by Fuzzer.
+    """
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return str(self.message)
+    def __repr__(self):
+        return repr(self.message)
+
+class TooHighForChr(GeneralException):
+    """
+    Raised to handle a value being too large for `chr`.
+    """
+    pass
