5d1b053a58b3f2a141409af61213079041058c03
ThatITNinja <ThatITNinja@hotmail.com>
2014-01-26 00:27:20 -0500

    fix description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='Fuzzer',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='A pure Python fuzz testing package.',
       author='ThatITNinja',
       url='https://github.com/ThatITNinja/Fuzzer',
       packages=['fuzzer', 'fuzzer.sqliteengine'],
