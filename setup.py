d8b2c79c1ebe6a253aaf6e03e3681fa3fd2c57d0
ThatITNinja <ThatITNinja@hotmail.com>
2014-01-26 00:25:24 -0500

    create setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Fuzzer',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='ThatITNinja',
+      url='https://github.com/ThatITNinja/Fuzzer',
+      packages=['fuzzer', 'fuzzer.sqliteengine'],
+     )
