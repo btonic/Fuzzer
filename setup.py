c5c48659b09d368902a33abaf72612955b7ac3fc
ThatITNinja <ThatITNinja@hotmail.com>
2014-02-13 16:42:30 -0500

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Fuzzer',
-      version='1.0',
+      version='1.1.0',
       description='A pure Python fuzz testing package.',
       author='ThatITNinja',
       url='https://github.com/ThatITNinja/Fuzzer',
