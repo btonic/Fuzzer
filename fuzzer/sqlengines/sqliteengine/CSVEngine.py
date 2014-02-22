a962e43f283669d495c7c1d9910d8141419a952b
ThatITNinja <ThatITNinja@hotmail.com>
2014-02-22 18:12:02 -0500

    begin working on sql parsing

--- fuzzer/sqlengines/sqliteengine/CSVEngine.py
+++ fuzzer/sqlengines/sqliteengine/CSVEngine.py
@@ -38,13 +38,41 @@ class SQLEngine(class):
 			#strip types, not needed
 			csv.write([table_name]+list(column[0] 
 				                        for column in columns))
+		csv.close()
 		#update tables definition
 		self.get_tables()
-		csv.close()
 	def read_query(self, query, *args):
 		"""
 		"""
-		pass
+		#This is a special function. Since csv does not use sql,
+		#we have to parse the sql and pull the necessary information.
+		#In order to do this, a tempfile will be used for ordering
+		held_results=[]
+		query = query.lower().split()
+		#Pull SELECT --->(values)<---... from query
+		select_values = query[(query.index("select")+1):query.index("from")]
+		#clean up the values
+		select_values = list(value.strip() for value in select_values.split(","))
+		#Pull FROM --->(table)<--- from query
+		if where_clause:
+			from_table = query[(query.index("from")+1):query.index("where")][0]
+		else:
+			from_table = query[(query.index("from")+1)]
+		#pull conditions
+		if where_clause and limit_clause:
+			where_conditions = query[(query.index("where")+1):query.index("limit")]
+			where_conditions = list(condition.split("=") for condition in where_conditions)
+		elif where_clause:
+			where_conditions = query[(query.index("where")+1):]
+			where_conditions = list(condition.split("=") for condition in where_conditions)
+		#pull order
+		if limit_clause:
+			order_by = query[(query.index("by")+1):query.index("limit")]
+		else:
+			order_by = query[(query.index("by")+1):]
+		#pull limit
+		if limit_clause:
+			limit = query[(query.index("limit")+1):][0].split(";")[0]
 	def append_to_pool(self, item, table_name):
 		"""
 		"""
